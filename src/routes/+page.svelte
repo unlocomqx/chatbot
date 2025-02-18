6454de31652e34a81af580ee6b986c249c3ef7c1
Tuni-Soft <amxmodx.unloco@gmail.com>
2025-02-18 08:08:28 +0100

    add shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<Button>Click me</Button>
